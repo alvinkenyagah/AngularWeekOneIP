0dd59e537ded6bd56b537600a52797dd598be82b
Alvinkenyagah <alvin47k@gmail.com>
2022-04-19 13:32:31 +0300

    update quotes.service.ts

--- src/app/services/quotes.service.ts
+++ src/app/services/quotes.service.ts
@@ -4,7 +4,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class QuotesService {
-  quotes: quote[] = [;]
+  quotes: quote[] = [
+    {
+      addedby: "Jadiel",
+      author: "Vladmir Putin",
+      quote: "Sometimes it is necessary to be lonely in order to prove that you are right." 
+
+
+    },
+    {
+      addedby: "Sally",
+      author: "Joe Biden",
+      quote: "Lets go Brandon!" 
+
+
+    },
+    {
+      addedby: "Joe",
+      author: "Elton john",
+      quote: "Hakuna Matata" 
+
+
+    },
+    {
+      addedby: "Roba",
+      author: "Safaricom-lady",
+      quote: "Samahani mteja wa nambari... " 
+
+
+    }
+  ];
 
   constructor() { }
 }
