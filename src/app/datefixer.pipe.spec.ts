e620372abc4773450a0a7fb8a29c3ee9916e481e
Alvinkenyagah <alvin47k@gmail.com>
2022-05-10 08:03:16 +0300

    Auto-generated commit [ci skip]

--- src/app/datefixer.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DatefixerPipe } from './datefixer.pipe';
-
-describe('DatefixerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DatefixerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/quotes.ts
+++
@@ -1,4 +0,0 @@
-export class Quotes {
-    
-    constructor(public body:string, public author:string, public submittedBy:string, public postedDate:Date){}
-}
